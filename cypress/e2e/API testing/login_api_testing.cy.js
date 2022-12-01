261f75d2e3f3bc89048e9682b15c51c7759d085a
Belén Montes <bmontes1067@gmail.com>
2022-12-01 13:27:01 +0100

    WIP
    Added the assertion on the response body
    Still pending:
    - Gherkin
    - POM

--- cypress/e2e/API testing/login_api_testing.cy.js
+++ cypress/e2e/API testing/login_api_testing.cy.js
@@ -1,5 +1,8 @@
 /// <reference types="cypress" />
 
+//TODO: Use Gherkin
+//TODO: Use POM
+
 beforeEach(() => {
     cy
         .visit('/');
@@ -25,15 +28,20 @@ it('test login', ()=>{
     cy
         .wait("@login")
         .then((login)=>{
+
             expect(login.response.statusCode).to.eq(200)
-            //TODO add response body
+            expect(login.response.body.accessToken).not.to.be.null
+            expect(login.request.body.email).to.eq("bmontes1067@gmail.com")
+            expect(login.request.body.password).to.eq("Aa123456")
         })
 
     cy
         .wait("@users")
         .then((users)=>{
             expect(users.response.statusCode).to.eq(200)
-            //TODO add response body
+            expect(users.response.body.user.email).to.eq("bmontes1067@gmail.com")
+            expect(users.response.body.user.password).not.to.be.null
+            expect(users.response.body.user.id).to.eq(1)
         })
 })
 it("failing password", ()=>{
@@ -63,6 +71,26 @@ it("failing email", ()=>{
 
     cy.get('[data-cy="login-menu"]').click()
     cy.get('[data-cy="login-email"]').type("test@gmail.com")
+    cy.get('[data-cy="login-password"]').type("Aa123456")
+    cy.get('[data-cy="login"]').click()
+
+    cy
+        .wait("@login")
+        .then((login)=>{
+            expect(login.response.statusCode).to.eq(400)
+            expect(login.response.body).to.eq("Cannot find user")
+        })
+
+})
+
+it("incorrect email format", ()=>{
+    cy.intercept({
+        method: 'POST',
+        url:'/login'
+    }).as("login")
+
+    cy.get('[data-cy="login-menu"]').click()
+    cy.get('[data-cy="login-email"]').type("test@gmail")
     cy.get('[data-cy="login-password"]').type("Aa123456")
     cy.get('[data-cy="login"]').click()
 
